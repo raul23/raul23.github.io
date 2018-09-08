909d54fc38fcae80bdb47bdd5f1c20acfb5bf656
Raul <rchfe23@gmail.com>
2018-09-08 18:47:09 -0400

    add bac to top arrow

--- assets/js/custom/back_to_top.js
+++ assets/js/custom/back_to_top.js
@@ -2,19 +2,21 @@
 ref.: https://bit.ly/2oThj8Y (How To Add Back To Top Button In Blogger)
 ref.: https://www.scrolltotop.com/ (Icon)
 */
-jQuery(document).ready(function() {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
+setTimeout(function() {
+    jQuery(window).ready(function () {
+        var offset = 220;
+        var duration = 500;
+        jQuery(window).scroll(function () {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.back-to-top').fadeIn(duration);
+            } else {
+                jQuery('.back-to-top').fadeOut(duration);
+            }
+        });
+        jQuery('.back-to-top').click(function (event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        });
     });
-    jQuery('.back-to-top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    });
-});
+}, 100);
---
+++ assets/js/custom/load_js_files.js
@@ -0,0 +1,23 @@
+/* *************************************************************
+ *  Loading js files.
+ *  ref.: https://stackoverflow.com/a/24641920
+ ************************************************************** */
+customPath ='/assets/js/custom/';
+distPath ='/dist/js/';
+vendorPath='/assets/js/vendor/';
+
+src = [
+    {f:'jquery-3.3.1.min.js', path:customPath},
+    {f:'popper.min.js', path:vendorPath},
+    {f:'bootstrap.min.js', path:distPath},
+    {f:'header.js', path:customPath},
+    {f:'back_to_top.js', path:customPath}
+];
+
+for (var i in src)
+{
+    document.write('<' + 'script type="text/javascript" src="' + src[i].path + src[i].f + '"></sc' + 'ript>');
+}
+
+// If the jQuery CDN loaded didn't work, load jQuery from website's resources folders
+window.jQuery || document.write('<script src="/assets/js/vendor/jquery-3.3.1.min.js" type="text/javascript"><\/script>')
