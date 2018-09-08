7371c4e3fa762c99493ace649058e6fcbc034d25
Raul <rchfe23@gmail.com>
2018-09-08 16:54:47 -0400

    add footer with ref, rename page

---
+++ assets/js/custom/back_to_top.js
@@ -0,0 +1,20 @@
+/* Back to top arrow
+ref.: https://bit.ly/2oThj8Y (How To Add Back To Top Button In Blogger)
+ref.: https://www.scrolltotop.com/ (Icon)
+*/
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+});
--- assets/js/custom/header.js
+++ assets/js/custom/header.js
@@ -21,14 +21,14 @@ function includeHTML() {
             /*make an HTTP request using the attribute value as the file name:*/
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                if (this.readyState === 4) {
+                    if (this.status === 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status === 404) {elmnt.innerHTML = "Page not found.";}
                     /*remove the attribute, and call this function once more:*/
                     elmnt.removeAttribute("w3-include-html");
                     includeHTML();
                 }
-            }
+            };
             xhttp.open("GET", file, true);
             xhttp.send();
             /*exit the function:*/
@@ -37,75 +37,13 @@ function includeHTML() {
     }
 }
 
-/*
-// TODO/NOTE: $(document).ready important
-// ref.: https://stackoverflow.com/a/27155910
-// TODO/NOTE: Not pure JavaScript, since it is using JQuery with $(document)
-// You could remove the $(document).ready function and make sure the JavaScript is loaded at then end of <body>
-$(document).ready(function(){
-    var menu = document.querySelectorAll('.menu li a');
-    for (var i = menu.length - 1; i >= 0; i--) {
-        if (menu[i].href==document.URL) {
-            // TODO/NOTE: remove all classes, and then set one class, ref.: https://stackoverflow.com/a/27155910
-            // menu[i].setAttribute("class", "active");
-            menu[i].classList.add("active");
-        }
-    };
-});
-*/
-
-// TODO/NOTE: with jQuery, ref.: https://stackoverflow.com/a/27155910
-//$(document).ready(function(){
-/*
-    $('.menu li a').on('click', function(event) {
-        event.preventDefault();
-        $('.menu li a').each(function() {
-            if ($(this).attr('href')==location.pathname) {
-                // TODO: why [0] on $(this)[0]?
-                if ($(this)[0].className.search("dropdown-item") == 0) {
-                    // TODO: if URL=http://localhost/p/projects/projects.html, make sure this if is not executed
-                    // TODO/NOTE: old code using parentNode twice
-                    //this.parentNode.parentNode.classList.add('active');
-                    $('#li_dropdown')[0].classList.add('active');
-                }
-                // TODO/NOTE: replaced $(this) with this unlike in the answer
-                $(this)[0].classList.add('active');
-            }
-            // If URL=https://raul23.github.io/ [root]
-            if (location.pathname=="/")
-                $('#home_link')[0].classList.add('active');
-        });
-    });
-*/
-//});
-
-/*
-$(window).on("load", function() {
-    // If URL=https://raul23.github.io/ [root]
-    if (location.pathname=="/") {
-        $('#home_link')[0].classList.add('active');
-    }
-    else {
-        $('li.active').removeClass('active');
-        $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
-    }
-});
-*/
-
-/*
-$(document).ready(function() {
-    $('a[href="' + this.location.pathname + '"]').parent().addClass('active');
-});
-*/
-
+/* Highlights the correct item in the dropdown menu from the navbar */
 setTimeout(function(){
-    //$('a[href="' + this.location.pathname + '"]').parent().addClass('active');
-
-    if (this.location.pathname=="/")
+    if (this.location.pathname==="/")
         $('#home_link').addClass('active');
     else {
         var elem = $('a[href="' + this.location.pathname + '"]');
-        if (elem[0].className.search("dropdown-item") == 0) {
+        if (elem[0].className.search("dropdown-item") === 0) {
             $('#li_dropdown').addClass('active');
             elem.addClass('active');
         }
