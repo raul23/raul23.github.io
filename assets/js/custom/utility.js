248157b8961b17e8745ae5ae5b4252a8067d566b
Raul <rchfe23@gmail.com>
2018-10-11 12:29:04 -0400

    fix hide li

--- assets/js/custom/utility.js
+++ assets/js/custom/utility.js
@@ -1,3 +1,5 @@
 maxwidth = $('html').css('max-width');
-if (maxwidth == '576px')
-    document.getElementById("hide_li_toc").style.display = "none";
+if (maxwidth == '576px'){
+    if (document.getElementById("hide_li_toc"))
+        document.getElementById("hide_li_toc").style.display = "none";
+}
