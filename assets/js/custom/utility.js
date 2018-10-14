f36bf6c67a2836ef1326d1fe6eefa3080f0f18eb
Raul <rchfe23@gmail.com>
2018-10-14 02:45:20 -0400

    hide scatter plots for smaller screens

--- assets/js/custom/utility.js
+++
@@ -1,5 +0,0 @@
-maxwidth = $('html').css('max-width');
-if (maxwidth == '576px'){
-    if (document.getElementById("hide_li_toc"))
-        document.getElementById("hide_li_toc").style.display = "none";
-}
