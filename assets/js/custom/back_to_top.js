f6f8ce205dc02230256f61a92fb7a53a89f5ea8d
Raul <rchfe23@gmail.com>
2020-09-16 22:33:28 -0400

    back_to_top.js: use 800 ms and go back to top if small screen else go to element

--- assets/js/custom/back_to_top.js
+++ assets/js/custom/back_to_top.js
@@ -3,7 +3,7 @@ ref.: https://bit.ly/2oThj8Y (How To Add Back To Top Button In Blogger)
 ref.: https://www.scrolltotop.com/ (Icon)
 */
 jQuery(window).ready(function () {
-    var offset = 220;
+    var offset = 800;
     var duration = 500;
     jQuery(window).scroll(function () {
         if (jQuery(this).scrollTop() > offset) {
@@ -14,7 +14,20 @@ jQuery(window).ready(function () {
     });
     jQuery('.back-to-top').click(function (event) {
         event.preventDefault();
-        jQuery('html, body').animate({scrollTop: $("#scroll-here").offset().top}, duration);
+        // Next lines sometimes adds # at the end of URL and you end up at the top of the page
+        // jQuery('html, body').animate({scrollTop: $("#scroll-here").offset().top}, duration);
+        // Reference for next line: https://stackoverflow.com/a/56687659
+        // document.getElementById("scroll-here").scrollIntoView( {behavior: "smooth" })
+        // $([document.documentElement, document.body]).animate({scrollTop: $("#scroll-here").offset().top}, duration);
+        // jQuery('html, body').stop().animate({scrollTop: $("#scroll-here").offset().top}, duration);
+        const mq = window.matchMedia( "(min-width: 577px)" );
+        if (mq.matches) {
+            // console.log("window width > 576px");
+            jQuery('html, body').animate({scrollTop: parseInt($("#scroll-here").offset().top)}, duration);
+        } else {
+            // console.log("window width <= 576px");
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+        }
         return false;
     });
 });
